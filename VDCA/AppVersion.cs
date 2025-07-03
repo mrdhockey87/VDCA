ff04fe492c716990b21e88b15bf141b24d834982
mrdhockey87 <mrdhockey@gmail.com>
2025-07-03 14:36:09 -0500

    v 6.040.0571 Update the licenses json file to include the new licenses for the CardsView.Maui plugin and the other plugins used in the app. mdail 7-3-25

--- VDCA/AppVersion.cs
+++ VDCA/AppVersion.cs
@@ -19,6 +19,8 @@
 }
 
 /*
+ * 
+ *  v 6.040.0571 Update the licenses json file to include the new licenses for the CardsView.Maui plugin and the other plugins used in the app. mdail 7-3-25
  *  v 6.040.0570 Changed where reset scroll to top is done in the CardView to OnItemAppearing &  OnItemDisappearing so that it works better with the cards
  *               because the scroll view load & unload happen when in the cashed cards. mdail 7-2-25
  *  v 6.040.0569 Need to test the scroll loaded & unloaded events to see if they work to reset the scrollview to the top mdail 7-2-25
